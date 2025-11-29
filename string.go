1dc554ebcfd5d9ecd575df9706d21901316b4523
MUZAAQI <muzaaqi@yahoo.com>
2025-11-29 12:08:26 +0700

    chore: Remake len and [] function in string data

--- string.go
+++ string.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("Muhammad")
+	fmt.Println("Zaki")
+	fmt.Println("As Shidiqi")
+
 	fmt.Println(len("Muhammad"))
 	fmt.Println("Zaki"[0])
-	fmt.Println("As Shidiqi"[1:5])
+	fmt.Println("As Shidiqi"[1])
 }
