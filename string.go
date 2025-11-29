c3f919ed2e3572ca51a2e34893b1b06ae9fe05e3
MUZAAQI <muzaaqi@yahoo.com>
2025-11-29 12:05:33 +0700

    chore: Use len and [] function in string data

--- string.go
+++ string.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Muhammad")
-	fmt.Println("Zaki")
-	fmt.Println("As Shidiqi")
+	fmt.Println(len("Muhammad"))
+	fmt.Println("Zaki"[0])
+	fmt.Println("As Shidiqi"[1:5])
 }
